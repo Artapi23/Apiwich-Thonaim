b411a3810a9c22ca8469867a3a68641a04c2e1b0
Apiwich <nemesiscrisisart@gmail.com>
2025-11-22 13:35:44 +0700

    a.2.c

---
+++ 1,3-test.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main() {
+    int num1, num2, operationCode, result;
+    
+    // รับค่าตัวเลข 2 ตัวและรหัสคำสั่ง
+    if (scanf("%d %d %d", &num1, &num2, &operationCode) != 3) {
+        return 1; // Handle input failure
+    }
+    
+    // TODO: Implement the switch statement logic here, including break and default
+
+
+    
+
+    return 0;
+}
