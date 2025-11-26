3dff98a2dc1ed877ebd348bf2c620a3dedaa47c1
Apiwich <nemesiscrisisart@gmail.com>
2025-11-26 15:19:55 +0700

    1.6

--- 1.6.c
+++ 1.6.c
@@ -1,1 +1,14 @@
-#
+#include <stdio.h>
+int main(){
+    int number;
+    do{
+        printf("Enter a number between 1 to 10:");
+        if(scanf("%d", &number) !=1){
+        }
+        if(number < 1 || number > 10){
+        printf("Error: value must be 1 -10:");
+        }
+    }while (number < 1 || number > 10);
+    printf("Input accepted: %d\n", number);
+    return 0;
+}
