f5a88429907add09f82cf61d248c225134a913ab
Apiwich <nemesiscrisisart@gmail.com>
2025-12-03 10:52:06 +0700

    lab1.2

---
+++ labadvance1.1/lab1.2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void Getset(int array[], int *num);
+int main(){
+    int N;
+    if(scanf("%d", &N) !=1 || N <=0){
+        printf("Invalid input\n");
+        return 1;
+    }
+    int num = N;
+    int data[num];
+    for(int i = 0; i < num; i++){
+        scanf("%d", &data[i]);
+    }
+    Getset(data, &num);
+    return 0;
+}
+void Getset(int array[], int *num){
+    for (int i = 0; i < *num; i++){
+        printf("%d",*(array+i));
+    }
+}
---
+++ labadvance1.1/labtest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define max_size 100
+void GetSet(int[],int *);
+int main(){
+    int *data;
+    int num;
+    Getset(data, &num);
+    if(num > 0){
+        printf(":", num);
+    }
+}
