c8341104318aa1a3990ae0da9fa9aa6e39c022ce
Apiwich <nemesiscrisisart@gmail.com>
2025-12-05 15:29:14 +0700

    1.11

--- Lab1/1.11.c
+++ Lab1/1.11.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
-
-int main()
-{
-    int N;
-    printf("Enter a number :");
-    if(scanf("%d",&N) != 1){
-        return 1;
+int main(){
+    int catagoryCode;
+    float price_before_vat;
+    float totalbill = 0.0;
+    float vatAmout = 0.0;
+    printf("Enter price before VAT:");
+    scanf("%f", &price_before_vat);
+    printf("Enter catagory code (1-4):");
+    scanf("%d", &catagoryCode);
+    if(catagoryCode == 1){
+        totalbill = price_before_vat * 1.07;
+    } else if(catagoryCode == 2){
+        totalbill = price_before_vat;
+    } else if(catagoryCode == 3){
+        totalbill = price_before_vat * 1.15;
+    } else {
+        printf("Invalid catagory code.\n");
+        totalbill = 0.0;
     }
-    if(N <= 0){
-        printf("Error");
-        return 1;
-    }
-    for (int i = 0; i < N; i++){
-        printf("Hello loop!\n");
-    }
+    if(totalbill > 0.0){
+        vatAmout = totalbill - price_before_vat;
+        printf("Total bill: %.2f bath\n", totalbill);
+        printf("VAT amount: %.2f bath\n", vatAmout);
+    } 
     return 0;
 }
