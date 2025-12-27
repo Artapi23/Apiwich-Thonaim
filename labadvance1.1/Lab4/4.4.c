36e85c381cd2cd215fc7188e2a37e552486ff1bf
Apiwich <nemesiscrisisart@gmail.com>
2025-12-27 16:58:29 +0700

    Lab_4.4

--- labadvance1.1/Lab4/4.4.c
+++ labadvance1.1/Lab4/4.4.c
@@ -11,10 +11,11 @@ struct studentNode
     struct studentNode *next ;
 } ;
     void saveNode( struct studentNode *child, char n[], int a, char s, float g ) ;
-    void Gonext( struct studentNode *walk ) ;
+    void Gonext( struct studentNode **walk ) ;
     
     int main(){
-        struct studentNode *start, *now1, *now2, *now3, *now4;
+        struct studentNode *start, *now1 ;
+        struct studentNode **now2 ;
 
         start = (struct studentNode *)malloc(sizeof(struct studentNode));
         saveNode(start, "BOB", 15, 'M', 3.22 );
@@ -27,16 +28,13 @@ struct studentNode
 
         start->next->next->next = (struct studentNode *)malloc(sizeof(struct studentNode));
         saveNode(start->next->next->next, "NEW", 18, 'F', 4.00);
+        start->next->next->next->next = NULL;
 
         now1 = start ;
-        now2 = start->next ;
-        now3 = start->next->next ;
-        now4 = start->next->next->next ;
+        now2 = &now1 ;
 
-        Gonext(now1);
+        Gonext(&now1);
         Gonext(now2);
-        Gonext(now3);
-        Gonext(now4);
         
         return 0;
     }
@@ -46,10 +44,10 @@ struct studentNode
         child->sex = s ;
         child->gpa = g ;
     }
-    void Gonext(struct studentNode *walk){
-        printf("Name: %s\n",walk->name);
-        printf("Age: %d\n",walk->age);
-        printf("Sex: %c\n",walk->sex);
-        printf("GPA: %.2f\n",walk->gpa);
-        walk = walk->next;
+    void Gonext(struct studentNode **walk){
+        printf("Name: %s\n",(*walk)->name);
+        printf("Age: %d\n",(*walk)->age);
+        printf("Sex: %c\n",(*walk)->sex);
+        printf("GPA: %.2f\n",(*walk)->gpa);
+        *walk = (*walk)->next;
     }
