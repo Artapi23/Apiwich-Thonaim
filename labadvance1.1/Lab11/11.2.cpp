20c26ca13792c2bbd050aaf1c4386eecd7dd2c55
Apiwich <nemesiscrisisart@gmail.com>
2026-03-06 22:21:13 +0700

    lab12.1

---
+++ labadvance1.1/Lab11/11.2.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void merfessort(int t[], int k);
+int main(){
+    int data[7] = {4,6,1,2,5,1,8} ;
+    int n = 7 ;
+    merfessort(data,n);
+    for(int i = 0; i < n; i++){
+        printf("%d ", data[i]);
+    }
+    return 0;
+}
+void merfessort(int t[], int k){
+    int i,j,minj,temp;
+    for(i = 0; i < k-1; i++){
+        minj = i ;
+        for(j = i+1; j < k; j++)
+            if(t[j] < t[minj])
+                minj = j ;
+        temp = t[i] ;
+        t[i] = t[minj] ;
+        t[minj] = temp ;
+        }
+}
+
+    
